cc4462890f3904ebe78808a95bcf9c4e259cd625
babasiker826 <98yudistira@gmail.com>
2025-09-18 14:53:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
+# HTML sayfaları
 @app.route("/")
 def index():
     return render_template("nabi-kimdir.html")
+
+@app.route("/nabi-kimdir")
+def nabi():
+    return render_template("nabi-kimdir.html")
+
+# Sitemap route'u
+@app.route("/sitemap.xml")
+def sitemap():
+    return send_from_directory(os.path.dirname(os.path.abspath(__file__)), "sitemap.xml")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
